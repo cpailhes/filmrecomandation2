b960ebac37ca4b80234aee2bb0f33c1add2e4def
Christophe <christophe_pailhes@yahoo.fr>
2021-05-28 09:50:23 +0200

    add

--- app.py
+++ app.py
@@ -16,23 +16,37 @@ def recommandation():
 
         #reading movie title given by user in the front-end
         Movie =request.args.get('movie')
-       
-        def recommend(m):
-            
-            cluster_KMeans=df_result[df_result.movie_title==m].cluster_KMeans.values
-            print(cluster_KMeans)
+        sample =request.args.get('sample')
+        if not sample :
+            sample="yes"
+        cluster_KMeans=df_result[df_result.movie_title==m].cluster_KMeans.values
+        if cluster_KMeans.size != 0 :
+                dfresult =df_result[df_result['cluster_KMeans']==cluster_KMeans[0]]    
+                dfresult =dfresult[df_result['movie_title']!=m]
+                
+        def recommend(m ,sample):
             if cluster_KMeans.size != 0 :
                 dfresult =df_result[df_result['cluster_KMeans']==cluster_KMeans[0]]    
                 dfresult =dfresult[df_result['movie_title']!=m]
-                dfresult = dfresult.sample(5)
-                dfresult = dfresult.reset_index()
-                return (dfresult[['movie_title']]).to_html()
-            else:
+                if sample == 'yes':
+                    dfresult = dfresult.sample(5)
+                    dfresult = dfresult.reset_index()
+                    return (dfresult[['movie_title']]).to_html()
+                else:
+                    dfresult1['note'] = 0
+                    for index, row in dfresult1.iterrows():
+                        row['note'] = gaussian_filter( row['tsne-2d-one'], row['tsne-2d-two'],10) 
+                    dfresult1.sort_values('note', ascending=False)
+                    dfresult1 = dfresult1.head(5)
+                    return (dfresult1[['movie_title']]).to_html()
+             else:
                 print('no movie')
                 return 'no movie found for :{}'.format(Movie)
+    
+
    #printing top-10 recommendations
         try:
-            output = recommend(Movie)
+            output = recommend(Movie ,sample)
             
             return output
         except ValueError as e:
